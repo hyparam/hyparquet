2590cbf97badf859ed06258b1bbf29a5384b52de
Kenny Daniel <platypii@gmail.com>
2023-12-29 11:08:36 -0800

    Stub implementation

---
+++ src/hyparquet.ts
@@ -0,0 +1,9 @@
+/**
+ * Read parquet data rows from a file
+ *
+ * @param arrayBuffer parquet file contents
+ * @returns array of rows
+ */
+export function parquetRead(arrayBuffer: ArrayBuffer): any[][] {
+  throw new Error('not implemented')
+}
