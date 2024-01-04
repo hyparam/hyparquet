b9efa711c3f6d023bdd4fd6777cce03195a2edab
Kenny Daniel <platypii@gmail.com>
2024-01-04 09:58:50 -0800

    jsdoc require types

--- src/hyparquet.ts
+++ src/hyparquet.ts
@@ -3,8 +3,8 @@ import { parquetMetadata } from './metadata.js'
 /**
  * Read parquet data rows from a file
  *
- * @param arrayBuffer parquet file contents
- * @returns array of rows
+ * @param {ArrayBuffer} arrayBuffer parquet file contents
+ * @returns {any[][]} row data
  */
 export function parquetRead(arrayBuffer: ArrayBuffer): any[][] {
   const metadata = parquetMetadata(arrayBuffer)
