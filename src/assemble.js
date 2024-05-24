af7bab33f8e83cfc4ccc4c8366f26f744c58a096
Kenny Daniel <platypii@gmail.com>
2024-05-23 17:27:43 -0700

    Handle top level repeated from duckdb#2557 :duck:

--- src/assemble.js
+++ src/assemble.js
@@ -49,7 +49,10 @@ export function assembleLists(
     currentContainer = containerStack.at(-1)
 
     // Go deeper to end of definition level
-    while (currentDepth < repetitionPath.length - 2 && (currentDefLevel < def || repetitionPath[currentDepth + 1] === 'REQUIRED')) {
+    while (
+      (currentDepth < repetitionPath.length - 2 || repetitionPath[currentDepth + 1] === 'REPEATED') &&
+      (currentDefLevel < def || repetitionPath[currentDepth + 1] === 'REQUIRED')
+    ) {
       currentDepth++
       if (repetitionPath[currentDepth] !== 'REQUIRED') {
         /** @type {any[]} */
