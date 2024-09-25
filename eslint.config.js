4f1b0bb74dbe29d92409804da5bd96c2ed640ac5
Kenny Daniel <platypii@gmail.com>
2024-09-25 00:18:42 -0700

    Upgrade eslint :sob:

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import javascript from '@eslint/js'
+import typescript from 'typescript-eslint'
+import jsdoc from 'eslint-plugin-jsdoc'
+import globals from 'globals'
+
+export default [
+  {
+    plugins: {
+      jsdoc,
+      typescript,
+    },
+
+    ignores: ['demo/**/*.min.js'],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      ...javascript.configs.recommended.rules,
+      ...typescript.configs.recommended.rules,
+      'arrow-spacing': 'error',
+      camelcase: 'off',
+      'comma-spacing': 'error',
+      'comma-dangle': ['error', {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      }],
+      'eol-last': 'error',
+      eqeqeq: 'error',
+      'func-style': ['error', 'declaration'],
+      indent: ['error', 2],
+      'jsdoc/check-param-names': 'error',
+      'jsdoc/check-property-names': 'error',
+      'jsdoc/check-tag-names': 'error',
+      'jsdoc/require-param': 'error',
+      'jsdoc/require-param-type': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-returns-type': 'error',
+      'jsdoc/sort-tags': 'error',
+      'no-constant-condition': 'off',
+      'no-extra-parens': 'error',
+      'no-multi-spaces': 'error',
+      'no-trailing-spaces': 'error',
+      'no-undef': 'error',
+      'no-unused-vars': 'error',
+      'no-useless-concat': 'error',
+      'no-useless-rename': 'error',
+      'no-useless-return': 'error',
+      'no-var': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'prefer-const': 'error',
+      'prefer-destructuring': ['warn', {
+        object: true,
+        array: false,
+      }],
+      'prefer-promise-reject-errors': 'error',
+      quotes: ['error', 'single'],
+      'require-await': 'warn',
+      semi: ['error', 'never'],
+      'sort-imports': ['error', {
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      }],
+      'space-infix-ops': 'error',
+    },
+  },
+]
