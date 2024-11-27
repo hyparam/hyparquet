8e62c07d9b9bdf22b2d9a2c3d96a77d81caccf3d
Sylvain Lesage <severo@rednegra.net>
2024-11-27 11:43:01 +0100

    Explicitly export types for use in downstream typescript projects

--- src/hyparquet.js
+++ src/hyparquet.js
@@ -12,7 +12,7 @@ export { asyncBufferFromFile, asyncBufferFromUrl, byteLengthFromUrl, toJson } fr
 export { cachedAsyncBuffer } from './asyncBuffer.js'
 
 /**
- * @param {import('../src/types.d.ts').ParquetReadOptions} options
+ * @param {ParquetReadOptions} options
  * @returns {Promise<Record<string, any>[]>} resolves when all requested rows and columns are parsed
 */
 export function parquetReadObjects(options) {
@@ -24,3 +24,41 @@ export function parquetReadObjects(options) {
     }).catch(reject)
   })
 }
+/**
+ * Explicitly export types for use in downstream typescript projects through
+ * `import { ParquetReadOptions } from 'hyparquet'` for example.
+ *
+ * @template {any} T
+ * @typedef {import('../src/types.d.ts').Awaitable<T>} Awaitable<T>
+ */
+
+/**
+ * @typedef {import('../src/types.d.ts').AsyncBuffer} AsyncBuffer
+ * @typedef {import('../src/types.d.ts').DataReader} DataReader
+ * @typedef {import('../src/types.d.ts').FileMetaData} FileMetaData
+ * @typedef {import('../src/types.d.ts').SchemaTree} SchemaTree
+ * @typedef {import('../src/types.d.ts').SchemaElement} SchemaElement
+ * @typedef {import('../src/types.d.ts').ParquetType} ParquetType
+ * @typedef {import('../src/types.d.ts').FieldRepetitionType} FieldRepetitionType
+ * @typedef {import('../src/types.d.ts').ConvertedType} ConvertedType
+ * @typedef {import('../src/types.d.ts').TimeUnit} TimeUnit
+ * @typedef {import('../src/types.d.ts').LogicalType} LogicalType
+ * @typedef {import('../src/types.d.ts').LogicalTypeType} LogicalTypeType
+ * @typedef {import('../src/types.d.ts').RowGroup} RowGroup
+ * @typedef {import('../src/types.d.ts').ColumnChunk} ColumnChunk
+ * @typedef {import('../src/types.d.ts').ColumnMetaData} ColumnMetaData
+ * @typedef {import('../src/types.d.ts').Encoding} Encoding
+ * @typedef {import('../src/types.d.ts').CompressionCodec} CompressionCodec
+ * @typedef {import('../src/types.d.ts').Compressors} Compressors
+ * @typedef {import('../src/types.d.ts').Statistics} Statistics
+ * @typedef {import('../src/types.d.ts').PageType} PageType
+ * @typedef {import('../src/types.d.ts').PageHeader} PageHeader
+ * @typedef {import('../src/types.d.ts').DataPageHeader} DataPageHeader
+ * @typedef {import('../src/types.d.ts').DictionaryPageHeader} DictionaryPageHeader
+ * @typedef {import('../src/types.d.ts').DecodedArray} DecodedArray
+ * @typedef {import('../src/types.d.ts').OffsetIndex} OffsetIndex
+ * @typedef {import('../src/types.d.ts').ColumnIndex} ColumnIndex
+ * @typedef {import('../src/types.d.ts').BoundaryOrder} BoundaryOrder
+ * @typedef {import('../src/types.d.ts').ColumnData} ColumnData
+ * @typedef {import('../src/types.d.ts').ParquetReadOptions} ParquetReadOptions
+ */
