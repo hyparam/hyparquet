611342d776acf432481836be376c06d8f5df0efa
Kenny Daniel <platypii@gmail.com>
2025-05-30 12:33:09 -0700

    Simplify asyncBufferFromFile

--- src/node.js
+++ src/node.js
@@ -3,37 +3,31 @@ import { createReadStream, promises as fs } from 'fs'
 export * from './hyparquet.js'
 
 /**
+ * @import {AsyncBuffer} from '../src/types.js'
+ */
+/**
  * Construct an AsyncBuffer for a local file using node fs package.
  *
  * @param {string} filename
  * @returns {Promise<AsyncBuffer>}
  */
 export async function asyncBufferFromFile(filename) {
-  const stat = await fs.stat(filename)
+  const { size } = await fs.stat(filename)
   return {
-    byteLength: stat.size,
-    async slice(start, end) {
+    byteLength: size,
+    slice(start, end) {
       // read file slice
-      const readStream = createReadStream(filename, { start, end })
-      return await readStreamToArrayBuffer(readStream)
+      const reader = createReadStream(filename, { start, end })
+      return new Promise((resolve, reject) => {
+        /** @type {any[]} */
+        const chunks = []
+        reader.on('data', chunk => chunks.push(chunk))
+        reader.on('error', reject)
+        reader.on('end', () => {
+          const buffer = Buffer.concat(chunks)
+          resolve(buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength))
+        })
+      })
     },
   }
-}
-
-/**
- * @import {AsyncBuffer} from '../src/types.js'
- * @param {import('stream').Readable} input
- * @returns {Promise<ArrayBuffer>}
- */
-function readStreamToArrayBuffer(input) {
-  return new Promise((resolve, reject) => {
-    /** @type {Buffer[]} */
-    const chunks = []
-    input.on('data', chunk => chunks.push(chunk))
-    input.on('end', () => {
-      const buffer = Buffer.concat(chunks)
-      resolve(buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength))
-    })
-    input.on('error', reject)
-  })
 }
