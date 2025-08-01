7a466e4c8233a6f1af2a1d8da51592c386c8660c
Sylvain Lesage <severo@rednegra.net>
2025-08-01 13:20:50 -0400

    remove breaking changes

--- src/index.js
+++ src/index.js
@@ -9,16 +9,15 @@ export { parquetRead }
 /**
  * This is a helper function to read parquet row data as a promise.
  * It is a wrapper around the more configurable parquetRead function.
- * @import {BaseParquetReadOptions} from '../src/types.d.ts'
- * @param {BaseParquetReadOptions} options
- * @returns {Promise<Record<string, any>[]>} resolves when all requested rows and columns are parsed.
+ * @param {Omit<ParquetReadOptions, 'onComplete'>} options
+ * @returns {Promise<Parameters<ObjectRowFormat['onComplete']>[0]>} resolves when all requested rows and columns are parsed.
  *   Depending on the rowFormat, this will be an array of arrays or an array of objects. The default is an array of objects.
 */
 export function parquetReadObjects(options) {
   return new Promise((onComplete, reject) => {
     parquetRead({
-      ...options,
       rowFormat: 'object',
+      ...options,
       onComplete,
     }).catch(reject)
   })
