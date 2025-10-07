421cb72a869984773b7a049085d9141c4c9db081
Sylvain Lesage <severo@rednegra.net>
2025-10-07 11:22:27 +0200

    convert crs

--- src/geoparquet.js
+++ src/geoparquet.js
@@ -1,7 +1,7 @@
 /**
  * @import {KeyValue, LogicalType} from '../src/types.d.ts'
  * @param {KeyValue[] | undefined} key_value_metadata
- * @returns {{ name: string; logical_type: LogicalType }[]}
+ * @returns {{ name: string; logical_type: LogicalType; }[]}
  */
 export function getGeoParquetColumns(key_value_metadata) {
   /** @type {{ name: string; logical_type: LogicalType }[]} */
@@ -9,16 +9,27 @@ export function getGeoParquetColumns(key_value_metadata) {
 
   const geo = key_value_metadata?.find(({ key }) => key === 'geo')?.value
   const decodedColumns = (geo && JSON.parse(geo)?.columns) ?? {}
-  for (const [name, { encoding, edges }] of Object.entries(decodedColumns)) {
-    if (encoding !== 'WKB') {
+  for (const [name, column] of Object.entries(decodedColumns)) {
+    if (column.encoding !== 'WKB') {
       continue
     }
-    const type = edges === 'spherical' ? 'GEOGRAPHY' : 'GEOMETRY'
-    /** @type {LogicalType} */
-    const logical_type = { type }
-    /* TODO(SL): extract 'crs' from column.crs */
-    /* Note: I don't think it's possible to get 'algorithm' from GeoParquet */
-    columns.push({ name, logical_type })
+    const type = column.edges === 'spherical' ? 'GEOGRAPHY' : 'GEOMETRY'
+    const crs = convertCrs(column.crs)
+    /* Note: we can't infer GEOGRAPHY's algorithm from GeoParquet */
+    columns.push({ name, logical_type: { type, crs } })
   }
   return columns
+}
+
+/**
+ * Convert crs property from GeoParquet metadata to crs field in Parquet LogicalType.
+ *
+ * @param {any} crs
+ * @returns {string | undefined}
+ */
+function convertCrs(crs) {
+  const id = crs?.id ?? crs?.ids?.[0]
+  if (id) {
+    return `${id.authority}:${id.code.toString()}`
+  }
 }
