35c6889790b87d76e619f440517633d74c78862a
dengribar <baryshok@astraloadhq.com>
2026-08-24 21:17:49 +0300

    Update "package.json" tests to handle "default" field in "exports"

--- test/package.test.js
+++ test/package.test.js
@@ -26,9 +26,9 @@ describe('package.json', () => {
     expect(Object.keys(exports)).toEqual(['.', './src/*.js'])
     // node vs default (browser)
     expect(Object.keys(exports['.'])).toEqual(['browser', 'default'])
-    expect(Object.keys(exports['.'].browser)).toEqual(['types', 'import'])
-    expect(Object.keys(exports['.'].default)).toEqual(['types', 'import'])
+    expect(Object.keys(exports['.'].browser)).toEqual(['types', 'import', 'default'])
+    expect(Object.keys(exports['.'].default)).toEqual(['types', 'import', 'default'])
     // deep imports
-    expect(Object.keys(exports['./src/*.js'])).toEqual(['types', 'import'])
+    expect(Object.keys(exports['./src/*.js'])).toEqual(['types', 'import', 'default'])
   })
 })
